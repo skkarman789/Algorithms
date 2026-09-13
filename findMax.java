231bf937c23eb1954630a29fe0c5f7008cd3200d
Arman Shaikh <skkarman789@gmail.com>
2026-09-13 23:13:17 +0530

    this is the basic code

--- findMax.java
+++ findMax.java
@@ -1,3 +1,13 @@
 public class findMax {
-    
+    public static void main(String[] args){
+        int max=Integer.MIN_VALUE;
+        int[] arr={1,4,7,8,3,89,9,3,34};
+        //task is to find the max without sorting 
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
